1705b9b806d20b726ae2dd1b24cf4be24c5c95f3
condebiel <bielbcm@gmail.com>
2023-05-17 12:54:55 +0200

    Add DB data

---
+++ src/data/db.js
@@ -0,0 +1,236 @@
+export default [
+  {
+    "name": "iPhone 13",
+    "brand": "Apple",
+    "price": 999,
+    "category": "Electronics"
+  },
+  {
+    "name": "Samsung Galaxy S21",
+    "brand": "Samsung",
+    "price": 899,
+    "category": "Electronics"
+  },
+  {
+    "name": "Sony PlayStation 5",
+    "brand": "Sony",
+    "price": 499,
+    "category": "Gaming"
+  },
+  {
+    "name": "Canon EOS R5",
+    "brand": "Canon",
+    "price": 3899,
+    "category": "Electronics"
+  },
+  {
+    "name": "Nike Air Max 270",
+    "brand": "Nike",
+    "price": 150,
+    "category": "Fashion"
+  },
+  {
+    "name": "Apple MacBook Pro",
+    "brand": "Apple",
+    "price": 1999,
+    "category": "Electronics"
+  },
+  {
+    "name": "Fitbit Versa 3",
+    "brand": "Fitbit",
+    "price": 229,
+    "category": "Electronics"
+  },
+  {
+    "name": "Amazon Echo Dot",
+    "brand": "Amazon",
+    "price": 49,
+    "category": "Electronics"
+  },
+  {
+    "name": "Dyson V11",
+    "brand": "Dyson",
+    "price": 599,
+    "category": "Home Appliances"
+  },
+  {
+    "name": "Adidas Ultraboost",
+    "brand": "Adidas",
+    "price": 180,
+    "category": "Fashion"
+  },
+  {
+    "name": "Samsung QLED TV",
+    "brand": "Samsung",
+    "price": 1499,
+    "category": "Electronics"
+  },
+  {
+    "name": "GoPro Hero 10",
+    "brand": "GoPro",
+    "price": 399,
+    "category": "Electronics"
+  },
+  {
+    "name": "Nintendo Switch",
+    "brand": "Nintendo",
+    "price": 299,
+    "category": "Gaming"
+  },
+  {
+    "name": "Bose QuietComfort 35 II",
+    "brand": "Bose",
+    "price": 299,
+    "category": "Electronics"
+  },
+  {
+    "name": "Microsoft Surface Pro 7",
+    "brand": "Microsoft",
+    "price": 899,
+    "category": "Electronics"
+  },
+  {
+    "name": "LG OLED TV",
+    "brand": "LG",
+    "price": 1999,
+    "category": "Electronics"
+  },
+  {
+    "name": "Levi's 501 Jeans",
+    "brand": "Levi's",
+    "price": 98,
+    "category": "Fashion"
+  },
+  {
+    "name": "Fitbit Charge 4",
+    "brand": "Fitbit",
+    "price": 149,
+    "category": "Electronics"
+  },
+  {
+    "name": "Nespresso VertuoPlus",
+    "brand": "Nespresso",
+    "price": 179,
+    "category": "Home Appliances"
+  },
+  {
+    "name": "Sony WH-1000XM4",
+    "brand": "Sony",
+    "price": 349,
+    "category": "Electronics"
+  },
+	{
+    "name": "Samsung Galaxy Watch 4",
+    "brand": "Samsung",
+    "price": 349,
+    "category": "Electronics"
+  },
+  {
+    "name": "Apple AirPods Pro",
+    "brand": "Apple",
+    "price": 249,
+    "category": "Electronics"
+  },
+  {
+    "name": "Sony A7 III",
+    "brand": "Sony",
+    "price": 1999,
+    "category": "Electronics"
+  },
+  {
+    "name": "Nike Zoom Pegasus 38",
+    "brand": "Nike",
+    "price": 120,
+    "category": "Fashion"
+  },
+  {
+    "name": "Bose SoundLink Revolve",
+    "brand": "Bose",
+    "price": 199,
+    "category": "Electronics"
+  },
+  {
+    "name": "Dell XPS 13",
+    "brand": "Dell",
+    "price": 1299,
+    "category": "Electronics"
+  },
+  {
+    "name": "Samsung Galaxy Buds Pro",
+    "brand": "Samsung",
+    "price": 199,
+    "category": "Electronics"
+  },
+  {
+    "name": "KitchenAid Stand Mixer",
+    "brand": "KitchenAid",
+    "price": 349,
+    "category": "Home Appliances"
+  },
+  {
+    "name": "Adidas Stan Smith",
+    "brand": "Adidas",
+    "price": 80,
+    "category": "Fashion"
+  },
+  {
+    "name": "Google Pixel 6",
+    "brand": "Google",
+    "price": 799,
+    "category": "Electronics"
+  },
+  {
+    "name": "Fitbit Inspire 2",
+    "brand": "Fitbit",
+    "price": 99,
+    "category": "Electronics"
+  },
+  {
+    "name": "LG NanoCell TV",
+    "brand": "LG",
+    "price": 1299,
+    "category": "Electronics"
+  },
+  {
+    "name": "Canon EF 50mm f/1.8",
+    "brand": "Canon",
+    "price": 125,
+    "category": "Electronics"
+  },
+  {
+    "name": "Vans Old Skool",
+    "brand": "Vans",
+    "price": 60,
+    "category": "Fashion"
+  },
+  {
+    "name": "Xbox Series X",
+    "brand": "Microsoft",
+    "price": 499,
+    "category": "Gaming"
+  },
+  {
+    "name": "Roomba i7+",
+    "brand": "iRobot",
+    "price": 799,
+    "category": "Home Appliances"
+  },
+  {
+    "name": "GoPro Hero 9",
+    "brand": "GoPro",
+    "price": 349,
+    "category": "Electronics"
+  },
+  {
+    "name": "Sony WH-1000XM3",
+    "brand": "Sony",
+    "price": 299,
+    "category": "Electronics"
+  },
+  {
+    "name": "JBL Flip 5",
+    "brand": "JBL",
+    "price": 119,
+    "category": "Electronics"
+  },
+]
